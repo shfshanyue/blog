6fe9a9f48ca8e007cb50cffa94ddeb2a85b52061
shfshanyue <xianger94@gmail.com>
2019-07-29 13:37:23 +0800

    Add favicon

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -2,6 +2,9 @@ module.exports = {
   base: '/',
   title: '山月行',
   description: '东坡云：事如春梦了无痕，技术积累也是如此，于是在此略作整理。浮生六记中有一句：见藐小微物，必细查其纹理，其中对技术不仅要如琢如磨如切如磋，也记录一些物外之趣。',
+  head: [
+    ['link', { rel: 'icon', href: '/favicon.ico' }]
+  ],
   themeConfig: {
     repo: 'shfshanyue/blog',
     nav: [
