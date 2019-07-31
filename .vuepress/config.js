a88083e5923ee1cb160335fb972dc368d505c04a
shfshanyue <xianger94@gmail.com>
2019-07-31 18:30:12 +0800

    Update icon

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -3,7 +3,7 @@ module.exports = {
   title: '山月行',
   description: '东坡云：事如春梦了无痕，技术积累也是如此，于是在此略作整理。浮生六记中有一句：见藐小微物，必细查其纹理，其中对技术不仅要如琢如磨如切如磋，也记录一些物外之趣。',
   head: [
-    ['link', { rel: 'icon', href: '/favicon.ico' }]
+    ['link', { rel: 'shortcut icon', href: '/favicon.ico', type: 'image/x-icon' }]
   ],
   themeConfig: {
     repo: 'shfshanyue/blog',
@@ -14,8 +14,10 @@ module.exports = {
       {
         text: '笔记', items: [
           { text: 'SQL', link: '/post/sql-guide/' },
-          { text: 'spark', link: '/post/learning-spark/' },
           { text: 'flutter', link: '/post/flutter-guide/' },
+          { text: 'Grid Layout', link: '/post/Grid-Guide/' },
+          { text: 'spark', link: '/post/learning-spark/' },
+          { text: 'scala', link: '/post/learning-scala/' },
         ]
       },
       { text: '关于我', link: '/about' },
