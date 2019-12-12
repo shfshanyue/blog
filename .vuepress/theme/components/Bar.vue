bbc7e3124d891a49a29a104db84b98defbe9b02a
shfshanyue <xianger94@gmail.com>
2019-12-12 21:49:35 +0800

    update

--- .vuepress/theme/components/Bar.vue
+++ .vuepress/theme/components/Bar.vue
@@ -5,7 +5,7 @@
         阿里云优惠活动，点击链接进行购买: <a href="https://www.aliyun.com/acts/hi-group-buying-share?ptCode=FDDDB2D258660CD9C81A96E25077465B89B9F59A2D8E8A59&userCode=4sm8juxu&activityId=3&share_source=copy_link">邀请你一起购买云服务器~~</a>
       </div>
       <div class="text">
-        你可以跟着我的笔记 <a href="https://shanyue.tech/op">当我有一台服务器时，我做了什么</a> 来开始维护服务器并搭建应用。
+        你可以跟着我的笔记 <a href="https://shanyue.tech/op/">当我有一台服务器时，我做了什么</a> 来开始维护服务器并搭建应用。
       </div>
     </div>
   </div>
