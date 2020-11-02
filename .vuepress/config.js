25a0be0c678f1af12cec2174fe70645613faf9cc
shfshanyue <xianger94@gmail.com>
2020-11-02 12:13:28 +0800

    update

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -37,7 +37,7 @@ module.exports = {
   description: '全栈成长之路，分享前后端以及 DevOps 相关文章，使各端开发者能够突破瓶颈进一步成长。',
   head: [
     ['link', { rel: 'shortcut icon', href: '/favicon.ico', type: 'image/x-icon' }],
-    ['meta', { name: 'baidu-site-verification', content: 'ZAdkE6LA10'}]
+    ['meta', { name: 'baidu-site-verification', content: 'ZAdkE6LA10'}],
     ['meta', { name: 'google-site-verification', content: '_rNB9Nt0ukzWmMfhXSSxCHUAeeMs24OiuhGm4QjdwXA'}] 
   ],
   themeConfig: {
